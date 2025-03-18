328bcfe8be2995c1ca2f61e435ccf794ded46f93
Leonardo Luis <72084769+leonardoluis1206@users.noreply.github.com>
2025-03-18 20:41:27 -0300

    ContadorPalavras.java

--- ContadorPalavras/src/main/java/com/mycompany/contadorpalavras/ContadorPalavras.java
+++ ContadorPalavras/src/main/java/com/mycompany/contadorpalavras/ContadorPalavras.java
@@ -9,13 +9,10 @@ public class ContadorPalavras {
         String texto = scanner.nextLine();
         scanner.close();
         
-        // Separar palavras ignorando pontuação e convertendo para minúsculas
         String[] palavras = texto.toLowerCase().replaceAll("[^a-zA-Z0-9 ]", "").split("\\s+");
         
-        // Usar um Set para armazenar palavras únicas
         Set<String> palavrasUnicas = new TreeSet<>(Arrays.asList(palavras));
-        
-        // Exibir resultados
+    
         System.out.println("Quantidade de palavras únicas: " + palavrasUnicas.size());
         System.out.println("Lista ordenada de palavras únicas: " + palavrasUnicas);
     }
